9ea602c1305240b8faae0235f591cfc5a34eea4c
M2596 <74707846+M2596@users.noreply.github.com>
2020-11-29 21:29:45 +0400

    Ygnd English
    Ygnd English

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -487,4 +487,4 @@
         $(e.target).parents('.form-group').find('.value')[0].innerHTML = e.target.value;
     });    
 
-})(jQuery);!function(){try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.style="display: none";a.innerHTML='<a href="https://mobirise.in">Mobirise Best Web Builder</a> Mobirise v5.2.0';document.body.insertBefore(a,document.body.childNodes[0])}}();
+})(jQuery);!function(){try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.style="display: none";a.innerHTML='<a href="https://mobirise.in">Mobirise Review</a> Mobirise v5.2.0';document.body.insertBefore(a,document.body.childNodes[0])}}();
